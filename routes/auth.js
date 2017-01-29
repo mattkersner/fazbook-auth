fe85d639a9cf5db6f6579c4e039fcfb41a31fa46
matt kersner <mattkersner@gmail.com>
2017-01-29 12:41:19 -0500

    added auth routes

---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const authHelpers = require('../auth/auth-helpers');
+const passport = require('../auth/local');
+
+router.get('/register', authHelpers.loginRedirect, (req, res) => {
+  res.render('auth/register');
+});
+
+router.post('/register', (req, res, next) => {
+  return authHelpers.createUser(req, res)
+  .then((response) => {
+    console.log('Registration successful');
+  })
+  .catch((err) => { res.status(500).json({ status: 'error' });
+  });
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/user',
+  failureRedirect: '/auth/login',
+  failureFlash: true
+ })
+);
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
