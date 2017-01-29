05f5d7999aa13813264913af82de32da802a58d4
matt kersner <mattkersner@gmail.com>
2017-01-29 12:44:35 -0500

    add user profile page route, loginRequired helper method

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -27,3 +27,16 @@ function createUser(req, res) {
     res.redirect('/');
   });
 }
+
+function loginRequired(req, res, next) {
+  if (!req.user) return res.status(401).json({ status: 'Please log in' });
+
+  return next();
+}
+
+module.exports = {
+  comparePass,
+  loginRedirect,
+  loginRequired,
+  createUser
+}
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,10 @@ const authHelpers = require('../auth/auth-helpers');
 
 /* GET user profile page. */
 // add route here
-
+router.get('/', authHelpers.loginRequired, (req, res, next) => {
+  res.render('user/index', {
+    user: req.user.dataValues
+  });
+});
 
 module.exports = router;
